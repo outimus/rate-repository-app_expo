5dd04cd8c9f1c42b74dc3726c7a11d2aa050fc4d
Outi Mustonen <outimus@gmail.com>
2023-01-25 16:14:06 +0200

    10.15 alustus

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,11 +1,12 @@
 import { useQuery } from '@apollo/client';
 import { GET_REPOSITORIES } from '../graphql/queries';
 
-
+//usequery ei hae tietoja miksi?
 const useRepositories = () => {
     const allRepositories = useQuery(GET_REPOSITORIES, {
         fetchPolicy: 'cache-and-network'
-    })
+        }
+    )
     
     const repositories = allRepositories.loading ? undefined : allRepositories.data.repositories
     const loading = allRepositories.loading;
--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,11 +1,15 @@
 import { useMutation } from "@apollo/client";
 import { AUTHENTICATE } from "../graphql/mutations";
+import useAuthStorage from '../hooks/useAuthStorage';
 
+//miten käytän authStoragea.
 const useSignIn = () => {
+    const authStorage = useAuthStorage();
     const [ mutate, result ] = useMutation(AUTHENTICATE);
 
     const signIn = async ({ username, password }) => {
         const data = await mutate({variables: {username, password }})
+        
         return data
 
     };
