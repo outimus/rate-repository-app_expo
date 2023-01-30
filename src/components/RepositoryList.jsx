c1d2f55fd0f909dbb3bd4901d2a5a575abfbcefe
Outi Mustonen <outimus@gmail.com>
2023-01-30 15:12:16 +0200

    osa_4 alustus

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -10,10 +10,9 @@ const styles = StyleSheet.create({
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
-const RepositoryList = () => {
-  const { repositories } = useRepositories();
-
+const RepositoryListContainer = ({ repositories }) => {
   const repositoryNodes = repositories
+  
     ? repositories.edges.map(edge => edge.node)
     : [];
   return (
@@ -25,6 +24,12 @@ const RepositoryList = () => {
       )}
     />
   );
+};
+
+const RepositoryList = () => {
+  const { repositories } = useRepositories();
+
+  return <RepositoryListContainer repositories={repositories} />;
 };
 
 export default RepositoryList;
